512b88e97572ab6f277f8ddc6085d5a6495fb683
Eduardo Gomes <edu1982talk@gmail.com>
2022-11-05 17:31:05 -0300

    Finish sale report

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,17 @@
 package com.devsuperior.dsmeta.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.dsmeta.dto.SalePeriodReportDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-
+	@Query("select s from Sale s where s.date between :startDate and :endDate and upper(s.seller.name) like concat('%', upper(:name), '%')")
+	Page<SalePeriodReportDTO> searchSaleForPeriodAndSellerName(LocalDate startDate, LocalDate endDate, String name,
+			Pageable pageable);
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,11 +1,18 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SalePeriodReportDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 
@@ -14,10 +21,31 @@ public class SaleService {
 
 	@Autowired
 	private SaleRepository repository;
-	
+
+	@Transactional(readOnly = true)
 	public SaleMinDTO findById(Long id) {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
 	}
+
+	@Transactional(readOnly = true)
+	public Page<SalePeriodReportDTO> searchSaleForPeriodAndSellerName(String minDate, String maxDate, String name, Pageable pageable) {
+		LocalDate startDate, endDate;
+
+		if (maxDate.equals(""))
+			endDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		else
+			endDate = LocalDate.parse(maxDate);
+
+		if (minDate.equals(""))
+			startDate = endDate.minusYears(1L);
+		else
+			startDate = LocalDate.parse(minDate);
+
+		Page<SalePeriodReportDTO> result = repository.searchSaleForPeriodAndSellerName(startDate, endDate, name, pageable);
+
+		return result;
+	}
+
 }
